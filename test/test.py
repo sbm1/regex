5cabbdc5ba0b9881b306c254156c46fe3866724c
sbm1 <112389331@umail.ucc.ie>
2017-03-30 23:56:11 +0100

    NFA refactored

--- test/test.py
+++ test/test.py
@@ -4,6 +4,7 @@ if ".." not in sys.path: sys.path.insert(0,"..")
 
 from regex.parse_tree import build_tree
 from regex.regex_parser import Regex
+from regex.nfa import build_nfa, print_nfa
 
 parser = Regex()
 
@@ -13,6 +14,8 @@ while 1:
         parser.run(input_expression)
         tree = build_tree(parser.tree)
         print(tree)
-        print(list(iter(tree)))
+        #print(list(iter(tree)))
+        nfa = build_nfa(tree)
+        print_nfa(nfa)
     except EOFError:
         break
